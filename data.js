8a1ce829441fd8270c097a9b1bc8cb15f2cc43c5
Christian Fischer <lanz33@gmail.com>
2024-01-04 13:30:54 +0100

    create JSON-Array and HTML

---
+++ data.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "rightAnswer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag &lt;a&gt?",
+        "answer_1": "Test Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "rightAnswer": 3
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer_1": "&lt;iframe;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe;",
+        "answer_3": "T&lt;frame;",
+        "answer_4": "&lt;frameset;",
+        "rightAnswer": 2
+    },
+    {
+        "question": "Wie stellt man Text am BESTEN fett dar?",
+        "answer_1": "&lt;strong&gt;",
+        "answer_2": "CSS nutzen",
+        "answer_3": "&lt;bold&gt;",
+        "answer_4": "&lt;b&gt;",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Welches Attribut kann man nicht für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt mit dem attribut title aus?",
+        "answer_1": "a[title]{...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}",
+        "answer_4": "a=title {...}",
+        "rightAnswer": 1
+    },
+    {
+        "question": "Wie definiert man in Javascript eine Variable?",
+        "answer_1": "let 100 = rate",
+        "answer_2": "100 = let rate",
+        "answer_3": "rate = 100",
+        "answer_4": "let rate = 100",
+        "rightAnswer": 4
+    },
+]
