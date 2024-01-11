a4001265d16c4fa0fc1a7211a5169731355c9a12
Christian Fischer <lanz33@gmail.com>
2024-01-11 12:36:38 +0100

    quiz done witout sound

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function init() {
+    document.getElementById('count-Total').innerHTML = questions.length;
     document.getElementById('countTotal').innerHTML = questions.length;
     showQuestion();
 }
@@ -8,20 +9,27 @@ let rightAnswers = 0;
 function showQuestion() {
     if (currentQuestion >= questions.length) {
         document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style ='display: none';
+        document.getElementById('questionBody').style = 'display: none';
         document.getElementById('countOfRightAnswers').innerHTML = rightAnswers;
         document.getElementById('header-image').src = './img/trophy.jpg';
-        rightAnswers=0;
+        rightAnswers = 0;
     }
     else {
-        document.getElementById('countActual').innerHTML = currentQuestion + 1;
-        let question = questions[currentQuestion];
-        document.getElementById('questionText').innerHTML = question["question"];
-        document.getElementById('answer_1').innerHTML = question["answer_1"];
-        document.getElementById('answer_2').innerHTML = question["answer_2"];
-        document.getElementById('answer_3').innerHTML = question["answer_3"];
-        document.getElementById('answer_4').innerHTML = question["answer_4"];
+        let percent = (currentQuestion + 1) / questions.length * 100;
+        document.getElementById('progress-bar').innerHTML = `${percent.toFixed(0)}%`;
+        document.getElementById('progress-bar').style.width = `${percent.toFixed(0)}%`;
+        nextQuestion();
     }
+}
+
+function nextQuestion() {
+    document.getElementById('countActual').innerHTML = currentQuestion + 1;
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question["question"];
+    document.getElementById('answer_1').innerHTML = question["answer_1"];
+    document.getElementById('answer_2').innerHTML = question["answer_2"];
+    document.getElementById('answer_3').innerHTML = question["answer_3"];
+    document.getElementById('answer_4').innerHTML = question["answer_4"];
 }
 
 function answer(selection) {
@@ -55,4 +63,13 @@ function resetAnswerButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restart() {
+    document.getElementById('header-image').src = './img/background.jpg';
+    document.getElementById('questionBody').style = '';
+    document.getElementById('endScreen').style = 'display: none';
+    currentQuestion = 0;
+    rightAnswers = 0;
+    init();
 }
